ac4093e1ed29707baa9f2e92ac4732317411ec7e
CARLVIN JERRY <carlvinjerry@gmail.com>
2024-08-27 16:56:15 +0300

    Add setup files

---
+++ Setup.R
@@ -0,0 +1,16 @@
+#Setup and installation of the library.
+#install.packages("devtools")
+devtools::install_github("nstrayer/datadrivencv")
+
+
+
+# run ?datadrivencv::use_datadriven_cv to see more details
+# datadrivencv::use_datadriven_cv(
+#   full_name = "Nick Strayer",
+#   data_location = "https://docs.google.com/spreadsheets/d/14MQICF2F8-vf8CKPF1m4lyGKO6_thG-4aSwat1e2TWc",
+#   pdf_location = "https://github.com/nstrayer/cv/raw/master/strayer_cv.pdf",
+#   pdf_location_resume = "https:///github.com/nstrayer/cv/raw/master/strayer_resume.pdf"
+#   html_location = "nickstrayer.me/cv/",
+#   html_location_resume = "nickstrayer.me/cv/resume/",
+#   source_location = "https://github.com/nstrayer/cv"
+# )
